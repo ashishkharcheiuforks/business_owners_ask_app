89e34978398d46f2464b84010062361ac45cbd38
iudragon <iudragon32@gmail.com>
2019-07-06 21:26:02 +0530

    master

---
+++ app/src/main/java/com/example/businessownersaskapptenk/DocumentationApp/DocumentationAppClass.java
@@ -0,0 +1,87 @@
+package com.example.businessownersaskapptenk.DocumentationApp;
+
+public class DocumentationAppClass {
+
+    /**
+     * Entry point for the app
+     * {@link com.example.businessownersaskapptenk.Activities.SignInActivity}
+     */
+
+    /**
+     * Create Empty Activity
+     * {@link com.example.businessownersaskapptenk.Activities.CustomerMainActivity}
+     */
+
+    /**
+     * Create Empty Activity
+     * {@link com.example.businessownersaskapptenk.Activities.CustomerMainActivity}
+     */
+
+    /**
+     * Implements Navigation Drawer by referring Android Developers Website
+     * {@link com.example.businessownersaskapptenk.Activities.CustomerMainActivity}
+     */
+
+    /**
+     * Create Fragment
+     * {@link com.example.businessownersaskapptenk.Fragments.RestaurantListFragment}
+     */
+
+    public class addRestaurantListFragmentToCustomerMainActivity {
+        /**
+         * Add {@link com.example.businessownersaskapptenk.Fragments.RestaurantListFragment}
+         * to {@link com.example.businessownersaskapptenk.Activities.CustomerMainActivity}
+         */
+    }
+
+    /**
+     * Create Fragment
+     * {@link com.example.businessownersaskapptenk.Fragments.TrayFragment}
+     */
+
+    /**
+     * Create Fragment
+     * {@link com.example.businessownersaskapptenk.Fragments.OrderFragment}
+     */
+
+    /**
+     * Create Empty Activity
+     * {@link com.example.businessownersaskapptenk.Activities.MealListActivity}
+     * Used when a RestaurantModel name is clicked to view the list of meals available
+     */
+
+    /**
+     * Create Empty Activity
+     * {@link com.example.businessownersaskapptenk.Activities.MealDetailActivity}
+     * Used when a MealModel name is clicked to view the details of that meal
+     */
+
+    /**
+     * Create Empty Activity
+     * {@link com.example.businessownersaskapptenk.Activities.PaymentActivity}
+     */
+
+    /**
+     * Create Empty Activity
+     * {@link com.example.businessownersaskapptenk.Activities.DriverMainActivity}
+     */
+
+    /**
+     * Create Fragment
+     * {@link com.example.businessownersaskapptenk.Fragments.OrderListFragment}
+     */
+
+    /**
+     * Create Fragment
+     * {@link com.example.businessownersaskapptenk.Fragments.DeliveryFragment}
+     */
+
+    /**
+     * Create Fragment
+     * {@link com.example.businessownersaskapptenk.Fragments.StatisticFragment}
+     */
+
+}
+
+
+
---
+++ app/src/main/java/com/example/businessownersaskapptenk/JsonModelObject/OrderDriverReady.java
@@ -0,0 +1,18 @@
+package com.example.businessownersaskapptenk.JsonModelObject;
+
+//
+//public class OrderDriverReady {
+//
+//
+//
+//    public void setOrders(ArrayList<OrderModel> orders) {
+//        this.orders = orders;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "OrderDriverReady{" +
+//                "orders=" + orders +
+//                '}';
+//    }
+//}
