5a9e8d34f4b2a88aca75f0ffb3ff0becd647ed9b
iudragon <iudragon32@gmail.com>
2019-07-13 00:14:32 +0530

    7

--- app/src/main/java/com/example/businessownersaskapptenk/Activities/PaymentActivity.java
+++ app/src/main/java/com/example/businessownersaskapptenk/Activities/PaymentActivity.java
@@ -45,19 +45,15 @@ public class PaymentActivity extends AppCompatActivity implements PaytmPaymentTr
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
         getSupportActionBar().setTitle("");
-
         Intent intent = getIntent();
         restaurantId = intent.getStringExtra("restaurantId");
         address = intent.getStringExtra("address");
         orderDetails = intent.getStringExtra("orderDetails");
-
         sharedPref = getSharedPreferences("MY_KEY", Context.MODE_PRIVATE);
         //creating random orderId and custId just for demonstration.
         //orderId = Helper.generateRandomString();
         //custid = Helper.generateRandomString();
-
         Log.d(TAG, "onCreate: Payment: " + orderId + " " + custid);
-
 //        final CardInputWidget mCardInputWidget = findViewById(R.id.card_input_widget);
         buttonPlaceOrder = findViewById(R.id.button_place_order);
         buttonPlaceOrder.setOnClickListener(new View.OnClickListener() {
@@ -66,38 +62,33 @@ public class PaymentActivity extends AppCompatActivity implements PaytmPaymentTr
                 if (ContextCompat.checkSelfPermission(PaymentActivity.this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
                     ActivityCompat.requestPermissions(PaymentActivity.this, new String[]{Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS}, 101);
                 }
-
                 //call this function on click event for BUY or CHECKOUT button
                 generateCheckSum();
-
-
             }
         });
     }
 
     private void generateCheckSum() {
         Log.d(TAG, "generateCheckSum: inside");
-
         String accessToken = sharedPref.getString("token", "");
         ApiService apiService = ApiClientFabCoding.getService();
-
         Call<ChecksumPaytm> call = apiService.getCustomerOrderAddPayment(accessToken, restaurantId, address, orderDetails);
-
+        Log.d(TAG, "generateCheckSum: orderDetails --> " + orderDetails);
+        Log.d(TAG, "generateCheckSum: accessToken --> " + accessToken);
+        Log.d(TAG, "generateCheckSum: restaurantId --> " + restaurantId);
+        Log.d(TAG, "generateCheckSum: address --> " + address);
+        Log.d(TAG, "generateCheckSum: before enqueue");
         call.enqueue(new Callback<ChecksumPaytm>() {
             @Override
             public void onResponse(Call<ChecksumPaytm> call, Response<ChecksumPaytm> response) {
-
                 if (response.body().getChecksumHash() != null) {
                     Log.d(TAG, "onResponse: Payment Activity: " + response.body());
-
                     ChecksumPaytm checksum = response.body();
-
                     Log.d(TAG, "onResponse: Payment Activity: " + response.body());
                     // when app is ready to publish use production service
                     PaytmPGService service1paytm = PaytmPGService.getStagingService();
                     // when app is ready to publish use production service
                     // PaytmPGService  Service = PaytmPGService.getProductionService();
-
                     //below parameter map is required to construct PaytmOrder object, Merchant should replace below map values with his own values
                     HashMap<String, String> paramMap = new HashMap<String, String>();
                     //these are mandatory parameters
@@ -107,33 +98,22 @@ public class PaymentActivity extends AppCompatActivity implements PaytmPaymentTr
                     paramMap.put("TXN_AMOUNT", checksum.getTnxAmount());
                     paramMap.put("CALLBACK_URL", checksum.getCall_back_url());
                     paramMap.put("CHECKSUMHASH", checksum.getChecksumHash());
-
                     paramMap.put("CHANNEL_ID", checksum.getChannel_id());//ne
-
                     paramMap.put("WEBSITE", checksum.getWebsite());//ne
                     paramMap.put("INDUSTRY_TYPE_ID", checksum.getIndustry_type_id());//ne
-
-
                     // paramMap.put( "EMAIL" , "abc@gmail.com");   // no need
                     //paramMap.put( "MOBILE_NO" , "999999999");  // no need
-
                     //paramMap.put("PAYMENT_TYPE_ID" ,"CC");    // no need
-
                     PaytmOrder Order = new PaytmOrder(paramMap);
                     Log.d(TAG, "onResponse: Payment Activity: " + response.body());
-
                     service1paytm.initialize(Order, null);
-
                     // start payment service call here
                     service1paytm.startPaymentTransaction(PaymentActivity.this, true, true, PaymentActivity.this);
                     Log.d(TAG, "onResponse: Payment Activity: " + response.body());
-                } else{
-                     
+                } else {
                     existingOrderError();
                 }
-
                 Log.d(TAG, "onResponse: checksum hash null check I am outside check");
-
             }
 
             @Override
@@ -145,11 +125,8 @@ public class PaymentActivity extends AppCompatActivity implements PaytmPaymentTr
 
     @SuppressLint("StaticFieldLeak")
     public void deleteTray() {
-
         final AppDatabase db = AppDatabase.getAppDatabase(PaymentActivity.this);
-
         new AsyncTask<Void, Void, Void>() {
-
             @Override
             protected Void doInBackground(Void... voids) {
                 db.trayDao().deleteAll();
@@ -160,47 +137,31 @@ public class PaymentActivity extends AppCompatActivity implements PaytmPaymentTr
 
     @Override
     public void onTransactionResponse(Bundle inResponse) {
-
         String orderId = inResponse.getString("ORDERID");
         Toast.makeText(PaymentActivity.this, "orderid" + orderId, Toast.LENGTH_SHORT).show();
-
-
         ApiService apiService = ApiClientFabCoding.getService();
         String accessToken = sharedPref.getString("token", "");
         Call<TransactionStatus> call = apiService.checkTransactionStatus(orderId, accessToken, restaurantId, address, orderDetails);
-
         call.enqueue(new Callback<TransactionStatus>() {
             @Override
             public void onResponse(Call<TransactionStatus> call, Response<TransactionStatus> response) {
-
-
                 if (!response.isSuccessful()) {
-
                     Log.d(TAG, "onResponse: onTransactionResponse response " + response.toString());
                     handleUnknownError();
                     return;
                 }
                 Log.d(TAG, "onResponse: after unsuccessful");
                 deleteTray();
-
                 Intent intent = new Intent(getApplicationContext(), CustomerMainActivity.class);
                 intent.putExtra("screen", "order");
                 startActivity(intent);
-
-
                 ///  if(response.body()==null){
-
                 //   return;
                 //}
                 TransactionStatus status = response.body();
                 Log.d(TAG, "onTransactionResponse: " + status);
-
-
                 Toast.makeText(PaymentActivity.this, "on payment" + status.getPaytStatus(), Toast.LENGTH_SHORT).show();
-
-
                 //showOrderStatus(false);
-
             }
 
             @Override
@@ -209,13 +170,9 @@ public class PaymentActivity extends AppCompatActivity implements PaytmPaymentTr
                 Log.d(TAG, "onFailure: FAILURE onTransactionResponse");
             }
         });
-
-
         // Toast.makeText(PaymentActivity.this, "Success on payment", Toast.LENGTH_SHORT).show();
         Toast.makeText(PaymentActivity.this, "Payment Transaction response " + inResponse.toString(), Toast.LENGTH_LONG).show();
         Log.d(TAG, "onTransactionResponse: ");
-
-
     }
 
     @Override
@@ -235,8 +192,6 @@ public class PaymentActivity extends AppCompatActivity implements PaytmPaymentTr
 
     @Override
     public void onErrorLoadingWebPage(int iniErrorCode, String inErrorMessage, String inFailingUrl) {
-
-
         Log.d(TAG, "onErrorLoadingWebPage: ");
     }
 
@@ -250,7 +205,6 @@ public class PaymentActivity extends AppCompatActivity implements PaytmPaymentTr
         Log.d(TAG, "onTransactionCancel: ");
     }
 
-
     public void handleUnknownError() {
         showErrorDialog(getString(R.string.msg_unknown));
     }
@@ -258,9 +212,6 @@ public class PaymentActivity extends AppCompatActivity implements PaytmPaymentTr
     public void existingOrderError() {
         showErrorDialog(getString(R.string.existing_order_error));
     }
-
-
-
 
     public void showErrorDialog(String message) {
         AlertDialog.Builder builder;
@@ -271,6 +222,4 @@ public class PaymentActivity extends AppCompatActivity implements PaytmPaymentTr
                 })
                 .show();
     }
-
-
 }
